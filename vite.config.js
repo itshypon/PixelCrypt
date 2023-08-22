586bda9bfbbc38607fc45b6b6bc2e43c79243e11
itshypon <himanshupoonia336@gmail.com>
2023-08-22 22:50:18 +0530

    Nuclear test

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+export default {
+    build: {
+      rollupOptions: {
+        input: {
+          index : 'index.html',
+          style : 'style.css',  
+          main: 'index.js', 
+          steganography: 'steganography.min.js'// Update with your main entry point
+          // Add other entry points if needed
+        }
+      }
+    }
+  }
+  
